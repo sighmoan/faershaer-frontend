763e3dbb4157c8813772b63b42e1a60441e50f53
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-16 16:51:56 +0200

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./App.css";
