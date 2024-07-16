551b61b98ca7fa7fcb39a2a2708b3e94880b6eb5
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-16 16:08:18 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import AddTxForm from "./AddTxForm";
 import TxList from "./TxList";
 import PersonBalancesList from "./PersonBalancesList";
+import Header from "./Header";
 import "./App.css";
 
 function App() {
   return (
-    <main className="mx-auto max-w-xl">
-      <AddTxForm />
-      <TxList></TxList>
-      <PersonBalancesList />
-    </main>
+    <>
+      <Header />
+      <main className="mx-auto max-w-xl">
+        <AddTxForm />
+        <TxList></TxList>
+        <PersonBalancesList />
+      </main>
+    </>
   );
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="mx-auto max-w-xl">
+        <h3 className="text-slate font-black italic">FÄRSHÄR</h3>
+      </div>
+      <div className="bg-primary hero min-h-60">
+        <h1 className="text-white text-4xl">Picnic @ Tantolunden</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
