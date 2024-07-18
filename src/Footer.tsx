4765f9b569ce2d0e8dee13038fecf2645d47a867
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-18 13:28:20 +0200

    add footer

---
+++ src/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="bg-primary mt-96 absolute inset-x-0 end-0 w-full">
+      <p className="text-primary text-center">faershaer</p>
+    </div>
+  );
+};
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,14 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import LogoStrip from "../LogoStrip";
+import { Footer } from "../Footer";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div className="min-h-screen">
       <LogoStrip />
       <Outlet />
-      <TanStackRouterDevtools />
-    </>
+      <Footer />
+    </div>
   ),
 });
